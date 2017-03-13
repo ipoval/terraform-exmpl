9f88b51bf6f7d172663ff01fb719cd63b3dd30df
Ivan Povalyukhin <ipovalyukhin@cli-26028.office.atlassian.com>
2017-03-14 10:49:33 +1100

    define IaaS config and spin up an instnace

---
+++ example.tf
@@ -0,0 +1,18 @@
+/* IaaS config */
+provider "aws" {
+  access_key = "AKIAJQGRSBW2TESPZ57A"
+  secret_key = "sDH1Yx6r4iEzB++Vw965dDogA/bvAtaW/lAY8HDQ"
+  region     = "us-west-1"
+}
+
+/* Spin an instance */
+resource "aws_instance" "example" {
+  ami           = "ami-16efb076"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-5bca4b03"
+}
+
+/* Assign Elastic IP */
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
