823c0f867f9b2b83ce13199f364be058fff5ad5c
Ivan Povalyukhin <ipovalyukhin@cli-26028.office.atlassian.com>
2017-03-14 12:50:56 +1100

    added input variables

--- example.tf
+++ example.tf
@@ -1,26 +1,32 @@
 /* IaaS config */
 provider "aws" {
-  access_key = "AKIAJQGRSBW2TESPZ57A"
-  secret_key = "sDH1Yx6r4iEzB++Vw965dDogA/bvAtaW/lAY8HDQ"
-  region     = "us-west-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
-resource "aws_instance" "another" {
+resource "aws_instance" "db_replica" {
   ami           = "ami-16efb076"
   instance_type = "t2.micro"
   subnet_id     = "subnet-5bca4b03"
 }
 
 /* Spin-up an instance */
-resource "aws_instance" "example" {
+resource "aws_instance" "db_master" {
   ami           = "ami-16efb076"
   instance_type = "t2.micro"
   subnet_id     = "subnet-5bca4b03"
+
+  /* basic provisioning */
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.db_master.public_ip} > db_master_ip.txt"
+  }
 }
 
 /* Assign Elastic IP */
 resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
+  instance = "${aws_instance.db_master.id}"
+  vpc      = true
   /* explicit dependency */
-  depends_on = ["aws_instance.example"]
+  depends_on = ["aws_instance.db_master"]
 }
